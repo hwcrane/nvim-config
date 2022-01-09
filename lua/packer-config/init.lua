3401e2640a91e61d59006c9e6c13f81df1f3ee2c
Harry Crane <37228043+hwcrane@users.noreply.github.com>
2022-01-09 20:12:34 +0000

    initial commit

---
+++ lua/packer-config/init.lua
@@ -0,0 +1,27 @@
+return require('packer').startup(function()
+    use 'wbthomason/packer.nvim'
+
+    -- Colour Scheme
+    use 'EdenEast/nightfox.nvim'
+
+    -- File tree
+    use 'kyazdani42/nvim-tree.lua'
+    use 'kyazdani42/nvim-web-devicons'
+
+    -- LSP
+    use 'neovim/nvim-lspconfig'
+    use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+    use 'L3MON4D3/LuaSnip' -- Snippets plugin
+    use 'onsails/lspkind-nvim'
+
+    -- Notifications
+    use 'rcarriga/nvim-notify'
+
+    -- Bottom line
+    use 'nvim-lualine/lualine.nvim'
+
+    -- Top Bar
+    use 'romgrk/barbar.nvim'
+end)
---
+++ lua/settings/init.lua
@@ -0,0 +1,26 @@
+local set = vim.opt
+
+vim.notify = require("notify")
+
+set.tabstop = 4 
+set.softtabstop = 4
+set.shiftwidth = 4
+set.expandtab = true
+set.smartindent = true
+set.hidden = true
+set.errorbells = false
+set.exrc = true
+set.swapfile = false
+set.backup = false
+set.undodir = "~/.vim/undodir"
+set.undofile = true
+set.incsearch = true
+set.scrolloff = 8
+set.relativenumber = true
+set.nu = true
+set.signcolumn = "yes"
+set.termguicolors = true
+set.splitbelow = true
+set.wrap = false
+set.splitright = true
+vim.o.mouse = 'a'
